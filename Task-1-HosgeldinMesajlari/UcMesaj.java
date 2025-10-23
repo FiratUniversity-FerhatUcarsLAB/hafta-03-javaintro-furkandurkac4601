ed018781b22f02ad54baba267a9c266ee1855bb0
furkandurkac4601 <furkandurkac4601@gmail.com>
2025-10-23 20:49:37 +0300

    Fix main method parameter name in UcMesaj.java

--- Task-1-HosgeldinMesajlari/UcMesaj.java
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -2,9 +2,10 @@ public class UcMesaj {
    public UcMesaj() {
    }
 
-   public static void main(String[] var0) {
+   public static void main(String[] args) {
       System.out.println("Javaya hoş geldinizi");
       System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
       System.out.println("Programlama Eğlencelidir");
    }
 }
+
