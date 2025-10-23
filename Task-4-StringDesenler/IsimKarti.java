15e40057c13f339afca820209f6ea7bdd83ba53f
furkandurkac4601 <furkandurkac4601@gmail.com>
2025-10-23 20:59:50 +0300

    Add IsimKarti class to display user information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,16 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Furkan ";
+        String soyad = Durkaç";
+        String no = "250541045";
+        String bolum = "Yazilim Mühendisliği";
+        System.out.println("************************************************");
+        System.out.println("*      İsim Karti            *");
+        System.out.println("************************************************");
+        System.out.println("* İsim Soyisim: *" + ad + " " + soyad);
+        System.out.println("* Numara:       *" + no);
+        System.out.println("* Bölüm:        *" + bolum);
+        System.out.println("************************************************");
+    
+    }
+}
