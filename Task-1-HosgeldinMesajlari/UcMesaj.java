5c1c83c15a58ca1a31beccaeb79b50a771d4fe16
furkandurkac4601 <furkandurkac4601@gmail.com>
2025-10-23 20:48:57 +0300

    Add UcMesaj class with welcome messages

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+public class UcMesaj {
+   public UcMesaj() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Javaya hoş geldinizi");
+      System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+      System.out.println("Programlama Eğlencelidir");
+   }
+}
